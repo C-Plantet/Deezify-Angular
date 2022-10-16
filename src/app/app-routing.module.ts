a5325a86f1a095b78c04087fd35af319e0a8a7df
C-Plantet <cplantet@enssat.fr>
2022-10-16 12:45:40 +0200

    Base login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: '', component: LoginPageComponent },
+    { path: 'connection', component: LoginPageComponent },
+    { path: 'inscription', component: LoginPageComponent },
+    { path: '**', component: AppComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
